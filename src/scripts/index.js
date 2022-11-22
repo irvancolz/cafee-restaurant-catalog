af9f10d093edb54b3d992500b01b07a65ae39085
Irvan Saharudin <irvansaharudin@gmail.com>
2022-11-22 08:18:50 +0700

    create hero-section

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/header.css';
+import '../styles/hero.css';
 
 console.log('Hello Coders! :)');
