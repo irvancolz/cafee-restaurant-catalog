268c5f5509ef2520550ac99575d9157a0af64adb
Irvan Saharudin <irvansaharudin@gmail.com>
2022-11-23 19:14:31 +0700

    create hotspots title sections

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,5 +2,6 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/header.css';
 import '../styles/hero.css';
+import '../styles/hotspots.css';
 
 console.log('Hello Coders! :)');
