411289a24a44187614649db2f99d76292bf49ee7
Irvan Saharudin <irvansaharudin@gmail.com>
2022-11-25 19:16:36 +0700

    create restaurants starter files

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,5 +3,6 @@ import '../styles/main.css';
 import '../styles/header.css';
 import '../styles/hero.css';
 import '../styles/hotspots.css';
+import '../styles/restaurants.css';
 
 console.log('Hello Coders! :)');
