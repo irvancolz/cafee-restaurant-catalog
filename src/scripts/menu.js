1117bcf913da3c256cdc62afb0bddfeae3c7aed6
Irvan Saharudin <irvansaharudin@gmail.com>
2022-11-30 05:33:05 +0700

    create animation on hamburger menu

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -8,9 +8,11 @@ menuBtn.addEventListener("click", (e) => {
   const isOpen = topnav.getAttribute("data-state");
   if (isOpen === "open") {
     topnav.setAttribute("data-state", "close");
+    menuBtn.setAttribute("data-state", "close");
     topnav.setAttribute("aria-expanded", "false");
     topnav.setAttribute("aria-hidden", "true");
   } else {
+    menuBtn.setAttribute("data-state", "open");
     topnav.setAttribute("data-state", "open");
     topnav.setAttribute("aria-expanded", "true");
     topnav.setAttribute("aria-hidden", "false");
