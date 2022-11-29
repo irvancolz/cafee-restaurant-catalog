06979aea1b82d9a2d64d988c21343d8ca92f1915
Irvan Saharudin <irvansaharudin@gmail.com>
2022-11-29 20:00:59 +0700

    create restaurant list starter script files

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,11 @@
-import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.css';
-import '../styles/header.css';
-import '../styles/hero.css';
-import '../styles/hotspots.css';
-import '../styles/restaurants.css';
-import '../styles/restaurant-list.css';
-import '../styles/footer.css';
-import '../styles/link.css';
-import "./menu"
+import "regenerator-runtime"; /* for async await transpile */
+import "../styles/main.css";
+import "../styles/header.css";
+import "../styles/hero.css";
+import "../styles/hotspots.css";
+import "../styles/restaurants.css";
+import "../styles/restaurant-list.css";
+import "../styles/footer.css";
+import "../styles/link.css";
+import "./menu";
+import "./restaurant-list";
