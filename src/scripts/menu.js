ac980496dd990c362b1598a6b4a007055c54ad10
Irvan Saharudin <irvansaharudin@gmail.com>
2022-11-30 04:48:09 +0700

    add aria hidden to menu

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -2,15 +2,17 @@
 
 const menuBtn = document.querySelector(".header__menu__btn");
 
-menuBtn.addEventListener("click",(e) =>{
-    e.preventDefault();
-    const topnav = document.getElementById("topnav");
-    const isOpen = topnav.getAttribute("data-state");
-    if(isOpen === "open"){
-        topnav.setAttribute("data-state", "close");
-        topnav.setAttribute("aria-expanded", "false");
-    }else{
-        topnav.setAttribute("data-state", "open");
-        topnav.setAttribute("aria-expanded", "true");
-    }
-})
+menuBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const topnav = document.getElementById("topnav");
+  const isOpen = topnav.getAttribute("data-state");
+  if (isOpen === "open") {
+    topnav.setAttribute("data-state", "close");
+    topnav.setAttribute("aria-expanded", "false");
+    topnav.setAttribute("aria-hidden", "true");
+  } else {
+    topnav.setAttribute("data-state", "open");
+    topnav.setAttribute("aria-expanded", "true");
+    topnav.setAttribute("aria-hidden", "false");
+  }
+});
