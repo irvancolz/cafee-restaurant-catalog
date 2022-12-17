b89868cc24312826125378bd658e8f8cf49bd5a0
Irvan Saharudin <irvansaharudin@gmail.com>
2022-12-17 13:51:40 +0700

    add restaurant list to favourite  pages

--- src/scripts/pages/favourite/index.js
+++ src/scripts/pages/favourite/index.js
@@ -1,12 +1,27 @@
 import GetRestaurant from "../../api/apicall";
+import createRestaurantCards from "../../helper/restaurant";
 
 const Favourite = {
   async render() {
-    return `<h1>Favourite Page</h1>`;
+    return `<section class="restaurant-list container" id="content">
+    <h1 class="restaurant-list__title title">places you might be <span>interested</span> in</h1>
+    <div class="resturant-list__container" id="restaurant-list"></div>
+  </section>`;
   },
   async afterRender() {
-    await GetRestaurant.list();
+    const res = await GetRestaurant.filterRestaurantWithRating(4.6);
+    this._createRestaurantListContent(res);
   },
+
+  _createRestaurantListContent(list) {
+    const restaurantListContainer = document.getElementById("restaurant-list");
+    list.forEach((resto) => {
+      const restaurant = document.createElement("article");
+      restaurant.classList.add("restaurants");
+      restaurant.innerHTML = createRestaurantCards(resto);
+      restaurantListContainer.append(restaurant);
+    });
+  }
 };
 
 export default Favourite;
