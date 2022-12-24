dc6274dd6375ecfe97f8d92d4d30f7d6be81f55c
Irvan Saharudin <irvansaharudin@gmail.com>
2022-12-24 14:40:20 +0700

    create favourite restaurant page

--- src/scripts/pages/favourite/favourite-page.js
+++ src/scripts/pages/favourite/favourite-page.js
@@ -3,14 +3,17 @@ import { createRestaurantCards } from "../../helper";
 
 export const Favourite = {
   async render() {
-    return `<section class="restaurant-list container" id="content">
-    <h1 class="restaurant-list__title title">places you might be <span>interested</span> in</h1>
+    return `
+    <custom-carousel interval="4"></custom-carousel>
+    <section class="restaurant-list container" id="content">
+    <h1 class="restaurant-list__title title">your <span>favourite</span> restaurant</h1>
     <div class="resturant-list__container" id="restaurant-list"></div>
   </section>`;
   },
   async afterRender() {
-    const res = await GetRestaurant.filterRestaurantWithRating(4.6);
+    const res = await GetRestaurant.filterRestaurantWithRating(4.5);
     this._createRestaurantListContent(res);
+    this._createCarouselContent(res);
   },
 
   _createRestaurantListContent(list) {
@@ -21,5 +24,10 @@ export const Favourite = {
       restaurant.innerHTML = createRestaurantCards(resto);
       restaurantListContainer.append(restaurant);
     });
-  }
+  },
+
+  _createCarouselContent(list) {
+    const carousel = document.querySelector("custom-carousel");
+    carousel.restaurantList = list;
+  },
 };
