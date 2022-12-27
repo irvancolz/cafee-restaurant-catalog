d343c0a6531d46fc059f5aae0d0a457519f97a50
Irvan Saharudin <irvansaharudin@gmail.com>
2022-12-27 20:53:04 +0700

    prevent chrome extension to be cached

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,10 +1,10 @@
+/* eslint-disable no-useless-return */
 import { cacheController } from "./helper";
 
 self.addEventListener("install", (event) => {
   // TODO: Caching App Shell Resource
   console.log("service worker installed");
-  caches.delete(cacheController.cache__name);
-  // event.waitUntil(cacheController._startCache());
+  event.waitUntil(cacheController._startCache());
 });
 self.addEventListener("activate", (event) => {
   // TODO: Delete old caches
@@ -13,8 +13,14 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-  // event.respondWith(
-  //   // cacheController._validateCache(event.request)
-  // );
+  if (
+    event.request.url.startsWith("chrome-extension") ||
+    event.request.url.includes("extension")
+  ) {
+    return;
+  }
+  event.respondWith(
+    cacheController._validateCache(event.request)
+  );
   // TODO: Add/get fetch request to/from caches
 });
