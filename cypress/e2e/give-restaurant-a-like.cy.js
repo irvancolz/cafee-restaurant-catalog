2b6edbf4ee847c17738f552c30f3676b3db1bde5
Irvan Saharudin <irvansaharudin@gmail.com>
2023-01-11 05:31:36 +0700

    refactor add and delete favourite restaurant test method

--- cypress/e2e/give-restaurant-a-like.cy.js
+++ cypress/e2e/give-restaurant-a-like.cy.js
@@ -3,16 +3,25 @@ const { deleteFromFavourite } = require("../../src/scripts/storage/db/db");
 /* eslint-disable no-undef */
 describe("like restaurant", () => {
   it("like a restaurant", async () => {
-    const id = "fnfn8mytkpmkfw1e867";
+    const id = "69ahsy71a5gkfw1e867";
     cy.on("uncaught:exception", () => {
       return false;
     });
     await deleteFromFavourite(id);
+    // visit the restaurant we want to like
     cy.visit(`http://localhost:8000/#/resto/${id}`);
-    cy.get('.favourite-btn').click();
+
+    // give restaurant a like
+    // add wait function because cypress command run too fast,
+    // this will prevent cypress to navigate to favourite page until
+    // the add favourite button is clicked
+    cy.get(".favourite-btn").wait(1000).trigger("click");
+
+    cy.visit("http://localhost:8000/#/favourite");
+
+    // check the favourite status of the latest restaurant added to favourite
+    cy.get(".restaurants__title a").last().click();
     cy.get(".favourite-btn")
-      .trigger('mouseover')
-      .trigger("click")
       .invoke("attr", "data-favourited")
       .should("eq", "true");
   });
--- cypress/e2e/i-dont-like-this-restaurant.cy.js
+++ cypress/e2e/i-dont-like-this-restaurant.cy.js
@@ -3,17 +3,27 @@ const { deleteFromFavourite } = require("../../src/scripts/storage/db/db");
 /* eslint-disable no-undef */
 describe("unlike restaurant", () => {
   it("unlike a restaurant", async () => {
+    const id = "69ahsy71a5gkfw1e867";
     cy.on("uncaught:exception", () => {
       return false;
     });
-    const id = "fnfn8mytkpmkfw1e867";
     await deleteFromFavourite(id);
+    // visit the restaurant we want to like
     cy.visit(`http://localhost:8000/#/resto/${id}`);
-    // cy.get(".favourite-btn").trigger("mouseover").click();
-    cy.get(".favourite-btn")
-      .trigger("mouseover")
-      .dblclick()
-      .invoke("attr", "data-favourited")
-      .should("eq", "false");
+
+    // give restaurant a like
+    // add wait function because cypress command run too fast,
+    // this will prevent cypress to navigate to favourite page until
+    // the add favourite button is clicked
+    cy.get(".favourite-btn").wait(1000).click();
+
+    cy.visit("http://localhost:8000/#/favourite");
+
+    // check the favourite status of the latest restaurant added to favourite
+    cy.get(".restaurants__title a").last().click();
+    cy.get(".favourite-btn").wait(1000).click();
+
+    cy.visit("http://localhost:8000/#/favourite");
+    cy.get(".restaurants__title a").should("have.length", 0);
   });
 });
