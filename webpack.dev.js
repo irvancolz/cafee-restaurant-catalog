f6098b96c9f96a04c791c131783aa2c52e8d0b67
Irvan Saharudin <irvansaharudin@gmail.com>
2023-01-16 11:00:16 +0700

    move bundle analyzer plugin to dev envronment

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,13 @@
-const { merge } = require('webpack-merge');
-const path = require('path');
-const common = require('./webpack.common');
+const { merge } = require("webpack-merge");
+const path = require("path");
+const common = require("./webpack.common");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
 module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
+  mode: "development",
+  devtool: "inline-source-map",
   devServer: {
-    static: path.resolve(__dirname, 'dist'),
+    static: path.resolve(__dirname, "dist"),
     open: true,
     client: {
       overlay: {
@@ -16,4 +17,5 @@ module.exports = merge(common, {
     },
     compress: true,
   },
+  plugins: [new BundleAnalyzerPlugin()],
 });
